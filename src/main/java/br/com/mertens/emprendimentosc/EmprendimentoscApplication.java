cd099c37346c86fdd973128f1a47a1817948d787
guilherme-at-298727505311 <gvmertens@gmail.com>
2026-02-25 19:09:20 -0300

    main | Start do projeto - initializr

---
+++ src/main/java/br/com/mertens/emprendimentosc/EmprendimentoscApplication.java
@@ -0,0 +1,13 @@
+package br.com.mertens.emprendimentosc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmprendimentoscApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmprendimentoscApplication.class, args);
+	}
+
+}
